e630ddbdcd930ab7ed481d07063d1cd62b3356f5
hrshojaie <hrshojaie@gmail.com>
2021-03-22 18:02:47 +0430

    working on api2

---
+++ src/Api2/Entities/Blog.cs
@@ -0,0 +1,23 @@
+﻿namespace Api2.Entities
+{
+    public class Blog
+    {
+        public Blog()
+        {
+
+        }
+
+        public Blog(int id, string title, string body)
+        {
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+    }
+}
---
+++ src/Api2/Entities/EntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Shared;
+using System.Collections.Generic;
+
+namespace Api2.Entities
+{
+    public class EntityConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder)
+        {
+            builder.Property(a => a.Title).IsRequired().HasMaxLength(512);
+            builder.Property(a => a.Body).IsRequired();
+
+            builder.HasData(new List<Blog>
+            {
+                new Blog(1001, string.Format(ServiceDefaultConfig.BlogTitle, 1), ServiceDefaultConfig.BlogBody),
+                new Blog(1002, string.Format(ServiceDefaultConfig.BlogTitle, 2), ServiceDefaultConfig.BlogBody),
+                new Blog(1003, string.Format(ServiceDefaultConfig.BlogTitle, 3), ServiceDefaultConfig.BlogBody),
+            });
+        }
+    }
+}
